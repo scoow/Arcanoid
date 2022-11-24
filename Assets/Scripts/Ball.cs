e853e851ee278b492b5bb49c804b205c5638b748
scoow <85620078+scoow@users.noreply.github.com>
2022-11-24 21:50:06 +0300

    Добавлен BallScript

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,18 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(CharacterController))]
 public class Ball : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    SerializeField]
+    private float _startSpeed;
+    [SerializeField]
+    private float _maxSpeed;
+    private float _speed = 0;
+
+    CharacterController _characterController;
+
+    private void Start()
     {
-        
+        _characterController = GetComponent<CharacterController>();
+    }
+    private void Update()
+    {
+        Move();
+    }
+    private void Move()
+    {
+        _characterController.Move(transform.TransformDirection(_speed * Time.deltaTime * Vector3.forward));
+    }
+    private void Launch()
+    {
+        _speed = _startSpeed;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
